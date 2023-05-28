4d2ff2ce0e16f99c1ca4f67f11e14c4661d9fb14
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-05-29 00:08:50 +0200

    Create 'is user logged in' system in navigation

--- views/nav/navigationView.php
+++ views/nav/navigationView.php
@@ -1,16 +1,39 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Quizzes</a>
-        </li>
-      </ul>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Quizzes</a>
+                </li>
+                <?php 
+                session_start();
+                $_SESSION['user-logged-in'] = true;
+                if(isset($_SESSION['user-logged-in'])) :
+                ?>
+                <li class = "nav-item">
+                    <a class = "nav-link" href = "#">Your account</a>
+                </li>
+                <li class = "nav-item">
+                    <a class = "nav-link" href = "#">Create quiz</a>
+                </li>
+                <?php 
+                    else :
+                ?>
+                <li class = "nav-item">
+                    <a class = "nav-link" href = "#">Sign up</a>
+                </li>
+                <li class = "nav-item">
+                    <a class = "nav-link" href = "#">Sign in</a>
+                </li>
+                <?php 
+                    endif;
+                ?>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
