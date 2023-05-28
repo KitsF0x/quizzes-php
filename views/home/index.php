35e272a87bd6aa611b2ddea429845dd1b893bec3
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-05-29 00:03:12 +0200

    Remove static import url

--- views/home/index.php
+++ views/home/index.php
@@ -1,12 +1,11 @@
 <html>
     <head>
-        <link rel="stylesheet" href="http://localhost/quizes/node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
         <title>Home</title>
 
     </head>
     <body class="container">
-        <script src="http://localhost/quizes/node_modules/bootstrap/dist/css/bootstrap.min.css" defer></script>
-        Home
+        <script src="../node_modules/bootstrap/dist/css/bootstrap.min.css" defer></script>
         <?php
         require_once '../controllers/NavController.php';
         $nav = new \controllers\NavController();
