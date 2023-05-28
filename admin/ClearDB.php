f81de164872cae93baaf951f3443ade7ba4c2431
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-05-29 01:13:58 +0200

    Create Init and Clear database options

---
+++ admin/ClearDB.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../config.php';
+$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+// Remove users table
+$sql = 'DROP TABLE `users`';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
---
+++ admin/InitDB.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../config.php';
+$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+// Create users table
+$sql = 'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;';
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
