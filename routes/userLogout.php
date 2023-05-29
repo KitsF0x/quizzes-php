e075345c4779e60c8bed6668bf649db435d8fbb1
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-05-30 00:11:52 +0200

    Remove previous auto-redirect from logout route

--- routes/userLogout.php
+++ routes/userLogout.php
@@ -1,9 +1,5 @@
 <?php
 @session_start(); 
-if($_SESSION['user-logged-in']) {
-    header("Location: index.php");
-    exit();
-}
 require_once ('../controllers/UserController.php');
 $userController = new controllers\UserController();
 $userController->logout();
