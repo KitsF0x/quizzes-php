9f6a7616f228a6c5e7b0cc6471bc73b4aea3d3fa
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-05-30 00:11:10 +0200

    Create auto-redirect when trying to route to user creation or login routes

--- routes/userStore.php
+++ routes/userStore.php
@@ -1,5 +1,9 @@
 <?php
 @session_start(); 
+if($_SESSION['user-logged-in']) {
+    header("Location: index.php");
+    exit();
+}
 require_once ('../controllers/UserController.php');
 $userController = new controllers\UserController();
 $userController->store($_POST);
