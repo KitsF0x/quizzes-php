3fa3cea976aa652dc85691eaf17073f3f09bc763
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-05-30 02:10:53 +0200

    Add dropdown menu in navbar

--- views/nav/navigationView.php
+++ views/nav/navigationView.php
@@ -6,34 +6,29 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Quizzes</a>
-                </li>
-                <?php 
-                if(isset($_SESSION['user-logged-in'])) :
-                ?>
-                <li class = "nav-item">
-                    <a class = "nav-link" href = "userProfile.php"><?php echo $_SESSION['user-nick'];?></a>
-                </li>
-                <li class = "nav-item">
-                    <a class = "nav-link" href = "userLogout.php">Logout</a>
-                </li>
-                <li class = "nav-item">
-                    <a class = "nav-link" href = "#">Create quiz</a>
-                </li>
-                <?php 
-                    else :
-                ?>
-                <li class = "nav-item">
-                    <a class = "nav-link" href = "userCreate.php">Sign up</a>
-                </li>
-                <li class = "nav-item">
-                    <a class = "nav-link" href = "userLoginForm.php">Sign in</a>
-                </li>
-                <?php 
-                    endif;
-                ?>
+            <ul class="navbar-nav ms-auto"> <!-- Dodano klasę 'ms-auto' -->
+                <?php if (isset($_SESSION['user-logged-in'])) : ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?php echo $_SESSION['user-nick']; ?>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="userProfile.php">Your profile</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="userLogout.php">Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="userCreate.php">Sign up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="userLoginForm.php">Sign in</a>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
