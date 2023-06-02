e470a762abda4add3a739a4233615e2c88ceb3f5
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-06-02 11:40:59 +0200

    Create question model

---
+++ models/Question.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace models;
+
+
+class Question {
+    private $id;
+    private $quizId;
+    private $text;
+    
+    	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getQuizId(){
+		return $this->quizId;
+	}
+
+	public function setQuizId($quizId){
+		$this->quizId = $quizId;
+	}
+
+	public function getText(){
+		return $this->text;
+	}
+
+	public function setText($text){
+		$this->text = $text;
+	}
+}
